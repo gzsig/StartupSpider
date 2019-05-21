efb1061a05d75df611f0584d2ccf2a7e8cf447d4
Gabriel Zsigmond <gaazsig@gmail.com>
2019-05-21 18:25:42 -0300

    getting StartSe social media and started startupBase crawler

--- StartSe/startSe_startups_individual_spider.py
+++ StartSe/startSe_startups_individual_spider.py
@@ -11,27 +11,52 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client.startupsDB
 startse = db.startse
 
+start = time.time()
+
 driver = webdriver.Chrome()
 
 cont = 0
 
-for startup in startse.find():
-  # if cont > 3:
-  #   break
+startups = startse.find({}, no_cursor_timeout=True)
+
+for startup in startups:
+#   if cont > 3:
+#     break
 
   cont+=1
   link = startup['startSe link']
   name = startup['name']
-  # old_website = startup['website']
 
   driver.get(link)
   time.sleep(random.randint(1,3))
   website = driver.find_elements_by_xpath("//a[@class='fn mr-15 text-link small']")
+  social_media = driver.find_elements_by_xpath("//a[@class='fn pr-15 text-link small']")
 
   if len(website) > 0:
     website = website[0].get_attribute('href')
     time.sleep(random.randint(1,2))
     startse.update_one({'name':name}, {'$set' : {'website': website}})
     print(str(cont) + '- {} website saved!'.format(name))
+  if len(social_media) > 0:
+    for link in social_media:
+      link = link.get_attribute('href')
+      if "facebook" in link:
+        startse.update_one({'name':name}, {'$set' : {'facebook': link}})
+        print('facebook: {} SAVED!'.format(link))
+      elif "linkedin" in link:
+        print('linkedin: {} SAVED!'.format(link))
+        startse.update_one({'name':name}, {'$set' : {'linkedin': link}})
+      elif "youtube" in link:
+        print('youtube: {} SAVED!'.format(link))     
+        startse.update_one({'name':name}, {'$set' : {'youtubre': link}})
+      elif "instagram" in link:
+        print('instagram: {} SAVED!'.format(link))       
+        startse.update_one({'name':name}, {'$set' : {'instagram': link}})
+      elif "twitter" in link:
+        print('twitter: {} SAVED!'.format(link))       
+        startse.update_one({'name':name}, {'$set' : {'twitter': link}})
 
+end = time.time()
+startups.close()
+print(end - start)
 driver.close()
---
+++ StartupBase/startupBase_startups_spider.py
@@ -0,0 +1,41 @@
+import json
+import pymongo
+from pymongo import MongoClient
+import random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# source env/bin/activate 
+# brew services start mongodb
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.startupsDB
+startupbase = db.startupbase
+
+driver = webdriver.Chrome()
+
+def search(url):
+  driver.get(url)
+  time.sleep(2)
+  driver.maximize_window()
+  time.sleep(3)
+
+  # names = driver.find_elements_by_xpath("//h6[@class='mb-0']")
+  # logo_links = driver.find_elements_by_css_selector(".col-4.sb-card__verified") 
+  # locations = driver.find_elements_by_xpath("//p[@class='text-muted']")
+  # infos = driver.find_elements_by_xpath("//small[@class='text-muted']")
+
+  names = driver.find_elements_by_tag_name('card-startup')
+  for i in range(len(names)):
+    # print(logo_links[i].find_element_by_tag_name('img').get_attribute('src'))
+    print(names[i].text)
+    print('\n')
+    # print(locations[i].text)
+    # print(infos[i].text)
+
+
+search('https://startupbase.abstartups.com.br/startups?refinementList%5Bbusiness_target%5D%5B0%5D=B2B')
+
+driver.close()
