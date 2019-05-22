1f200a0310095dc01accfc0845b198d28f8c9a6a
Gabriel Zsigmond <gaazsig@gmail.com>
2019-05-22 11:47:01 -0300

    seems to be working with online db

--- StartupBase/startupBase_startups_spider.py
+++ StartupBase/startupBase_startups_spider.py
@@ -1,6 +1,7 @@
 import json
 import pymongo
 from pymongo import MongoClient
+import dns # required for connecting with SRV
 import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,9 +11,9 @@ import time
 # source env/bin/activate 
 # brew services start mongodb
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.startupsDB
-startupbase = db.startupbase
+client = pymongo.MongoClient("mongodb+srv://gabriel:Gz%40db3611@cluster0-ozy7g.mongodb.net/test?retryWrites=true")
+db = client.test
+test_one = db.test_one
 
 driver = webdriver.Chrome()
 
@@ -21,34 +22,43 @@ def search(url):
   time.sleep(2)
   driver.maximize_window()
   time.sleep(3)
+  for n in range(1):
+    startups = driver.find_elements_by_tag_name('card-startup')
+    for i in range(len(startups)):
+      if startups[i].find_element_by_css_selector(".mb-0") != None:
+        name = startups[i].find_element_by_css_selector(".mb-0").text
+      else:
+        name = '-'
+      if startups[i].find_element_by_css_selector(".col-4.sb-card__verified") != None:
+        logo = startups[i].find_element_by_css_selector(".col-4.sb-card__verified").find_element_by_tag_name('img').get_attribute('src')
+      else:
+        logo = '-'
+      if startups[i].find_element_by_tag_name('small') != None:
+        location = startups[i].find_element_by_tag_name('small').text
+      else:
+        location = '-'
+      if len(startups[i].find_elements_by_class_name('text-center')) > 0:
+        info = startups[i].find_element_by_class_name('text-center').text
+      else:
+        info = '-'
+      if test_one.find_one({'name' : str(name)}) == None and str(name) != '':
+        data = {
+          'name' : str(name),
+          'location' : str(location),
+          'logo' : str(logo),
+          'info' : str(info)
+        }
+        print('{} saved!'.format(name))
+        print('{} saved!'.format(logo))
+        print('{} saved!'.format(location))
+        print('{} saved!'.format(name))
+        print('\n')
+        test_one.insert_one(data)
+      else:
+        print('{} already!'.format(name))
+      time.sleep(random.randint(1,3))
 
-  # names = driver.find_elements_by_xpath("//h6[@class='mb-0']")
-  # logo_links = driver.find_elements_by_css_selector(".col-4.sb-card__verified") 
-  # locations = driver.find_elements_by_xpath("//p[@class='text-muted']")
-  # infos = driver.find_elements_by_xpath("//small[@class='text-muted']")
-
-  names = driver.find_elements_by_tag_name('card-startup')
-  for i in range(len(names)):
-    # print(logo_links[i].find_element_by_tag_name('img').get_attribute('src'))
-    if names[i].find_element_by_css_selector(".mb-0") != None:
-      print('name')
-      print(names[i].find_element_by_css_selector(".mb-0").text)
-
-    if names[i].find_element_by_css_selector(".col-4.sb-card__verified") != None:
-      print('logo')
-      print(names[i].find_element_by_css_selector(".col-4.sb-card__verified").find_element_by_tag_name('img').get_attribute('src'))
-
-    if names[i].find_element_by_tag_name('small') != None:
-      print('location')
-      print(names[i].find_element_by_tag_name('small').text)
-
-    if len(names[i].find_elements_by_class_name('text-center')) > 0:
-      print('info')
-      print(names[i].find_element_by_class_name('text-center').text)
-    
-    print('\n')
-    # print(locations[i].text)
-    # print(infos[i].text)
+    driver.find_element_by_xpath('/html/body/app-root/app-list/div[2]/div/div/div[2]/div[1]/pagination/ul/li[8]/a').click()
 
 
 search('https://startupbase.abstartups.com.br/startups?refinementList%5Bbusiness_target%5D%5B0%5D=B2B')
