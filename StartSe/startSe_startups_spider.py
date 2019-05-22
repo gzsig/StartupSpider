f3e7d2091176c843c8c6b1bac6ea9bea133f1806
Gabriel Zsigmond <gaazsig@gmail.com>
2019-05-22 14:39:23 -0300

    optimized

--- StartSe/startSe_startups_spider.py
+++ StartSe/startSe_startups_spider.py
@@ -1,6 +1,7 @@
 import json
 import pymongo
 from pymongo import MongoClient
+import dns # required for connecting with SRV
 import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,77 +9,69 @@ import time
 
 
 
-# Working
-
 # source env/bin/activate 
 # brew services start mongodb
 
-client = MongoClient('mongodb://localhost:27017/')
+client = pymongo.MongoClient("mongodb+srv://gabriel:Gz%40db3611@cluster0-ozy7g.mongodb.net/test?retryWrites=true")
 db = client.startupsDB
-startse = db.startse
+startSe = db.startSe
 
 driver = webdriver.Chrome()
 
-driver.get('https://comunidade.startse.com/startups')
-time.sleep(3)
-driver.maximize_window()
-time.sleep(3)
-
-i=0
-
-while len(driver.find_elements_by_class_name('infinite-more-link')) > 0: #pay attention: find_element*s*
-  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-  time.sleep(1)
-
-  # i+=1
-  # if i > 0:
-  #   break
-
-
 start = time.time()
 
-names = driver.find_elements_by_xpath("//h5[@class='fb m-0 p-lines h5-lines-2']")
-links = driver.find_elements_by_css_selector('.infinite-item')
-types = driver.find_elements_by_xpath("//p[@class='small text-link p-lines p-lines-1 mt-5 mb-0']")
-locations = driver.find_elements_by_xpath("//p[@class='small text-muted p-lines p-lines-1 m-0']")
-images = driver.find_elements_by_class_name("mb-10")
+def search(url):
+  driver.get(url)
+  time.sleep(1)
+  driver.maximize_window()
+  time.sleep(3)
+  i=0
+  while len(driver.find_elements_by_class_name('infinite-more-link')) > 0: #pay attention: find_element*s*
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    time.sleep(1)
+    i+=1
+    if i > 2:
+      break
 
-# if len(names) > 0:
-#   for i in range(len(names)):
-#     print(i)
-#     print(names[i].text)
-#     print(links[i].find_element_by_tag_name('a').get_attribute('href'))
-#     print(types[i].text)
-#     print(locations[i].text)
-#     print(images[i].get_attribute("src"))
-# else:
-#   print('something went wrong :(')
+  startups = driver.find_elements_by_xpath("//div[@class='infinite-item mix col-md-2 col-sm-3 col-xs-6 search']")
+  for i in range(len(startups)):
+    # print(startups[i].find_element_by_tag_name('a').get_attribute('href'))
+    # print(startups[i].find_element_by_tag_name('img').get_attribute('src'))
+    # print(startups[i].find_element_by_css_selector(".fb.m-0.p-lines.h5-lines-2").text)
+    # print(startups[i].find_element_by_css_selector('.small.text-muted.p-lines.p-lines-1.m-0').text)
+    # print('\n')
 
-
-cont = 0
-
-while cont < len(names):
-  name = names[cont].text
-  startSe_link = links[cont].find_element_by_tag_name('a').get_attribute('href')
-  company_type = types[cont].text
-  location = locations[cont].text
-  s3_image_link = images[cont].get_attribute("src")
-  if startse.find_one({'name' : str(name)}) == None and str(name) != '':
-    data = {
-      'name' : str(name),
-      'startSe link' : str(startSe_link),
-      'company Type' : str(company_type),
-      'location' : str(location),
-      's3 logo link' : str(s3_image_link),
-    }
-    startse.insert_one(data)
-    print(str(cont) + '- {} saved!'.format(name))
-    print("\n")
-    cont+=1
-  else:
-    print(str(cont) + '- {} already exists in StartSe collection!'.format(name))
-    print("\n")
-    cont+=1
+    if len(startups[i].find_elements_by_tag_name('a')) > 0:
+      internal_link = startups[i].find_element_by_tag_name('a').get_attribute('href')
+    else:
+      internal_link = '-'
+    if len(startups[i].find_elements_by_tag_name('img')) > 0:
+      logo = startups[i].find_element_by_tag_name('img').get_attribute('src')
+    else:
+      logo = '-'
+    if len(startups[i].find_elements_by_css_selector(".fb.m-0.p-lines.h5-lines-2")) > 0:
+      name = startups[i].find_element_by_css_selector(".fb.m-0.p-lines.h5-lines-2").text
+    else:
+      name = '-'
+    if len(startups[i].find_elements_by_css_selector('.small.text-muted.p-lines.p-lines-1.m-0')) > 0:
+      location = startups[i].find_element_by_css_selector('.small.text-muted.p-lines.p-lines-1.m-0').text
+    else:
+      location = '-'
+    
+    if startSe.find_one({'name' : str(name)}) == None and str(name) != '':
+      data = {
+        'name' : str(name),
+        'location' : str(location),
+        'logo' : str(logo),
+        'internal link' : str(internal_link)
+      }
+      startSe.insert_one(data)
+      print('{} saved!'.format(name))
+      print('\n')
+    else:
+      print('{} already exists in DB!'.format(name))
+      
+search('https://comunidade.startse.com/startups')
 
 end = time.time()
 print(end - start)
