5a61433bbb49fe02f5b0b9fd65d53ed7ed5a5377
Gabriel Zsigmond <gaazsig@gmail.com>
2019-05-24 10:58:16 -0300

    added deal book folder

---
+++ DealBook/dealBook_startups_individual_spider.py
@@ -0,0 +1,42 @@
+import json
+import pymongo
+from pymongo import MongoClient
+import dns # required for connecting with SRV
+import random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# source env/bin/activate 
+# brew services start mongodb
+
+client = pymongo.MongoClient("mongodb+srv://gabriel:Gz%40db3611@cluster0-ozy7g.mongodb.net/test?retryWrites=true")
+db = client.startupsDB
+dealBook = db.dealBook
+
+driver = webdriver.Chrome()
+
+start = time.time()
+
+cont = 0
+
+for startup in dealBook.find():
+  if cont > 3:
+    break
+  cont += 1
+  link = startup['internal link']
+  name = startup['name']
+  driver.get(link)
+  
+  table = driver.find_element_by_tag_name('tbody')
+  rows = table.find_elements_by_tag_name('tr')
+
+  for row in rows:
+    
+
+  
+
+end = time.time()
+print(end - start)
+driver.close()
