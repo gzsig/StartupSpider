1957b120ebaf3ed805dd81be7afb7a1f5b23eae3
Gabriel Zsigmond <gaazsig@gmail.com>
2019-05-28 19:44:01 -0300

    prints gathered data

--- join_data.py
+++ join_data.py
@@ -3,6 +3,9 @@ from pymongo import MongoClient
 import dns
 import time
 
+from collections import OrderedDict
+from operator import itemgetter   
+
 # Getting acces to database
 password = ""
 with open("password.txt", "r") as password_file:
@@ -18,4 +21,16 @@ angelList = db.angelList
 dealBook = db.dealBook
 startSe = db.startSe
 startupBase = db.startupBase
+cont = 0
+for startup in startupBase.find():
+  name = startup['name']
+  for startse in startSe.find({'name' : name}):
+    startup.update(startse)
+  for angellist in angelList.find({'name' : name}):
+    startup.update(angellist)
+
+  cont += 1
+  print(cont)
+  print(startup)
+  print('\n')
 
