a30889a8bfbe9e78b0c658489345a8f5b223da3d
Gabriel Zsigmond <gaazsig@gmail.com>
2019-05-30 15:14:16 -0300

    everything working and added coments

--- StartupBase/StartupBase_fixDB.py
+++ StartupBase/StartupBase_fixDB.py
@@ -23,24 +23,77 @@ startupBase = db.startupBase
 # starting to count time to get full duration of run time
 start = time.time()
 
-# cont = 0
-# for startup in startupBase.find({'info' : ''}):
-#   name = startup['name']
-#   cont += 1
-#   startupBase.update_one({'name' : name}, {'$unset' : {'info': 1}})
-#   print(cont)
-#   print(startup)
-#   print('\n')
+# update infos
+cont = 0
+for startup in startupBase.find({'info' : ''}):
+  name = startup['name']
+  cont += 1
+  startupBase.update_one({'name' : name}, {'$unset' : {'info': 1}})
+print('updated {} infos!'.format(cont))
+print('\n')
 
+# update locations
 cont = 0
 for startup in startupBase.find({'location' : '-'}):
   name = startup['name']
   cont += 1
   startupBase.update_one({'name' : name}, {'$unset' : {'location': 1}})
-  print(cont)
-  print(startup)
-  print('\n')
+print('updated {} locations!'.format(cont))
+print('\n')
 
+# update markets
+cont = 0
+for startup in startupBase.find({'market' : '-'}):
+  name = startup['name']
+  cont += 1
+  startupBase.update_one({'name' : name}, {'$unset' : {'market' : '-'}})
+print('updated {} markets!'.format(cont))
+print('\n')
+
+# update foundations
+cont = 0
+for startup in startupBase.find({'foundation' : '(Não informado)'}):
+  name = startup['name']
+  cont += 1
+  startupBase.update_one({'name' : name}, {'$unset' : {'foundation' : '(Não informado)'}})
+print('updated {} foundations!'.format(cont))
+print('\n')
+
+# update target audience
+cont = 0
+for startup in startupBase.find({'target audience' : 'S/N'}):
+  name = startup['name']
+  cont += 1
+  startupBase.update_one({'name' : name}, {'$unset' : {'target audience' : 'S/N'}})
+print('updated {} target audience!'.format(cont))
+print('\n')
+
+# update moment
+cont = 0
+for startup in startupBase.find({'moment' : 'S/N'}):
+  name = startup['name']
+  cont += 1
+  startupBase.update_one({'name' : name}, {'$unset' : {'moment' : 'S/N'}})
+print('updated {} moment!'.format(cont))
+print('\n')
+
+# update revenue model
+cont = 0
+for startup in startupBase.find({'revenue model' : 'S/N'}):
+  name = startup['name']
+  cont += 1
+  startupBase.update_one({'name' : name}, {'$unset' : {'revenue model' : 'S/N'}})
+print('updated {} revenue model!'.format(cont))
+print('\n')
+
+# update employees
+cont = 0
+for startup in startupBase.find({'employees' : '(não informado)'}):
+  name = startup['name']
+  cont += 1
+  startupBase.update_one({'name' : name}, {'$unset' : {'employees' : '(não informado)'}})
+print('updated {} employees!'.format(cont))
+print('\n')
 
 # gets the end time and print the duration
 end = time.time()
